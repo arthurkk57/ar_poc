c4ba36ba886234c5d7a2ec73df2f4131e96ad1e9
Arthur <xhk18@163.com>
2023-02-25 16:40:41 +0800

    test

--- poc/component.js
+++ poc/component.js
@@ -13,15 +13,20 @@ AFRAME.registerComponent('spawn-flowers', {
                 z: -25
             });
             // Add a click event listener to the flower.
-            flower.el.addEventListener('click', function () {
+            flower.addEventListener('click', function () {
                 alert("clicked");
+                flower.setAttribute('position', {
+                    x: -2,// Math.random() * 6 - 3,
+                    y: -2,// Math.random() * 4 - 2,
+                    z: -25
+                });
                 // Increment the score by 1.
                 const score = document.querySelector('#score');
                 const currentScore = parseInt(score.getAttribute('text').value.split(' ')[1]);
                 // score.setAttribute('value', 'Score: ' + (currentScore + 1));
                 score.setAttribute('text', 'value: Score: 9999; color: #F0F0F0');
                 // Remove the flower entity.
-                this.parentNode.removeChild(this);
+                // this.parentNode.removeChild(this);
             });
             // Add the flower to the gameplay entity.
             document.querySelector('#gameplay').appendChild(flower);
