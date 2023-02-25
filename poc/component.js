1ca6ee3defb575826e04f83d4d6be35291e16f8c
Arthur <xhk18@163.com>
2023-02-25 15:17:18 +0800

    test

---
+++ poc/component.js
@@ -0,0 +1,40 @@
+AFRAME.registerComponent('spawn-flowers', {
+  init: function () {
+    const self = this;
+    const mtlLoader = new THREE.MTLLoader();
+    mtlLoader.load('cube.mtl', function (materials) {
+      materials.preload();
+      const loader = new THREE.OBJLoader();
+      objLoader.setMaterials(materials);
+      loader.load('cube.obj', function (object) {
+        self.flowerModel = object;
+        // Set up a 10-second timer to spawn new flowers.
+        self.interval = setInterval(function () {
+          // Create a new flower entity.
+          const flower = document.createElement('a-entity');
+          flower.setObject3D('mesh', self.flowerModel.clone());
+          // Set the flower's position to a random location.
+          flower.setAttribute('position', {
+            x: 0,// Math.random() * 6 - 3,
+            y: 1,// Math.random() * 4 - 2,
+            z: -2
+          });
+          // Add a click event listener to the flower.
+          flower.addEventListener('click', function () {
+            // Increment the score by 1.
+            const score = document.querySelector('#score');
+            const currentScore = parseInt(score.getAttribute('value').split(' ')[1]);
+            score.setAttribute('value', 'Score: ' + (currentScore + 1));
+            // Remove the flower entity.
+            this.parentNode.removeChild(this);
+          });
+          // Add the flower to the gameplay entity.
+          document.querySelector('#gameplay').appendChild(flower);
+        }, 100);
+      });
+    });
+  },
+  remove: function () {
+    clearInterval(this.interval);
+  }
+});
