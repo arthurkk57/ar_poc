6c5a8647ad63043ef2d28e21dfb69fe8fc1b7e4b
Arthur <xhk18@163.com>
2023-02-26 22:10:19 +0800

    test

--- poc/component.js
+++ poc/component.js
@@ -1,6 +1,6 @@
 AFRAME.registerComponent('spawn-flowers', {
     init: function () {
-        console.log('Version 1');
+        console.log('## Version 2');
         const self = this;
 
         self.interval = setInterval(() => {
@@ -10,10 +10,10 @@ AFRAME.registerComponent('spawn-flowers', {
             var flowerModel = document.createElement('a-entity');
             flowerModel.setAttribute('gltf-model', '#flower-glb');
             flowerModel.setAttribute('id', 'flower');
-            const xPre = Math.random() * 2 - 1;
+            const xPre = Math.random() < 0.5 ? -1 : 1;
             const x = xPre * (Math.random() * 30 + 20);
             const y = Math.random() * 15 + 10;
-            const zPre = Math.random() * 2 - 1;
+            const zPre = Math.random() < 0.5 ? -1 : 1;
             const z = zPre * (Math.random() * 30 + 20);
             //-25;//Math.floor(Math.random() * 50) - 25;
             // const y = Math.floor(Math.sqrt(25 * 25 - x * x - z * z));
