c5a9c2b782e0d5e8f7394087998e5d890bde5273
Arthur <xhk18@163.com>
2023-02-28 21:17:29 +0800

    test

--- poc/component.js
+++ poc/component.js
@@ -1,7 +1,19 @@
 AFRAME.registerComponent('spawn-flowers', {
     init: function () {
-        console.log('## Version 16');
+        console.log('## Version 17');
         const self = this;
+
+        var takePhotoButton = document.querySelector("#takePhotoBtn");
+      takePhotoButton.addEventListener("click", function () {
+        var canvas = document.querySelector("canvas");
+        var dataURL = canvas.toDataURL("image/png");
+        var link = document.createElement("a");
+        link.download = "ar_poc_screenshot.png";
+        link.href = dataURL;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      });
 
         self.interval = setInterval(() => {
             // const flower = document.querySelector('#flower');
@@ -36,43 +48,9 @@ AFRAME.registerComponent('spawn-flowers', {
             flowerModel.setAttribute('animation__rotate', "property: rotation; to: 0 360 0; loop: true; dur: 5000");
             document.querySelector('a-scene').appendChild(flowerModel);
         }, 300);
-        /*
-                var scoreNum = 0;
-                self.interval = setInterval(function () {
-                    // Create a new flower entity.
-                    const flower = document.createElement('a-entity');
-                    // flower.setObject3D('mesh', self.flowerModel.clone());
-                    flower.setAttribute('obj-model', "obj: url(cube.obj); mtl: url(cube.mtl)");
-                    // Set the flower's position to a random location.
-                    flower.setAttribute('position', {
-                        x: 2,// Math.random() * 6 - 3,
-                        y: 2,// Math.random() * 4 - 2,
-                        z: -25
-                    });
-                    flower.addEventListener('isPlaying', function () { console.log("isPlaying")} );
-                    flower.addEventListener('hasLoaded', function () { console.log("hasLoaded")} );
-                    // Add a click event listener to the flower.
-                    flower.addEventListener('click', function () {
-                        console.log("clicked");
-                        flower.setAttribute('position', {
-                            x: -2,// Math.random() * 6 - 3,
-                            y: -2,// Math.random() * 4 - 2,
-                            z: -25
-                        });
-                        // Increment the score by 1.
-                        const score = document.querySelector('#score');
-                        const currentScore = parseInt(score.getAttribute('text').value.split(' ')[1]);
-                        // score.setAttribute('value', 'Score: ' + (currentScore + 1));
-                        score.setAttribute('text', 'value: Score: 9999; color: #F0F0F0');
-                        // Remove the flower entity.
-                        // this.parentNode.removeChild(this);
-                    });
-                    // Add the flower to the gameplay entity.
-                    document.querySelector('#gameplay').appendChild(flower);
-                }, 100);
-                */
     },
     remove: function () {
         clearInterval(this.interval);
     }
 });
+
