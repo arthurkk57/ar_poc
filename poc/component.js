661330e39d46ebd043952e60164aff742bc4f03c
Arthur <xhk18@163.com>
2023-03-01 00:36:02 +0800

    test

--- poc/component.js
+++ poc/component.js
@@ -1,56 +1,7 @@
-// import floorPic from 'assets/floorPic.json' assert { type: 'JSON' };
-
 AFRAME.registerComponent('spawn-flowers', {
     init: function () {
-        console.log('## Version 34');
+        console.log('## Version 35');
         const self = this;
-
-        //     var takePhotoButton = document.querySelector("#takePhotoBtn");
-        //   takePhotoButton.addEventListener("click", function () {
-        //     var canvas = document.querySelector("canvas");
-        //     var dataURL = canvas.toDataURL("image/png");
-        //     var link = document.createElement("a");
-        //     link.download = "ar_poc_screenshot.png";
-        //     link.href = dataURL;
-        //     document.body.appendChild(link);
-        //     link.click();
-        //     document.body.removeChild(link);
-        //   });
-        /*
-                self.interval = setInterval(() => {
-                    // const flower = document.querySelector('#flower');
-                    // flower.parentNode.removeChild(flower);
-        
-                    var flowerModel = document.createElement('a-entity');
-                    const modelId = Math.floor(Math.random() * 4) + 1;
-                    flowerModel.setAttribute('gltf-model', '#flower-' + modelId);
-                    // console.log('modelId: ' + modelId);
-                    const id = Math.random().toString(36).substring(7);
-                    flowerModel.setAttribute('id', id);
-                    setTimeout(function () {
-                        const flower = document.getElementById(`${id}`); // 通过 ID 获取花的实体
-                        // console.log('getElementById: ' + id);
-                        if (flower) {
-                            // console.log('removeChild: ' + id);
-                            // flower.parentNode.removeChild(flower); // 移除花的实体
-                        }
-                    }, 5000);
-        
-                    const r = 50;
-                    const y = Math.random() * 25;
-                    const x = Math.random() * 2 * r - r;
-                    const zPre = Math.random() < 0.5 ? -1 : 1;
-                    const z = zPre * (Math.sqrt(r * r - x * x));
-                    flowerModel.setAttribute('position', x + ' ' + y + ' ' + z);
-                    flowerModel.setAttribute('scale', '0.8 0.8 0.8');
-                    flowerModel.setAttribute('material', 'opacity:1');
-                    flowerModel.setAttribute('rotation', '0 0 0');
-                    flowerModel.setAttribute('animation__rotate', "property: rotation; to: 0 360 0; loop: true; dur: 5000");
-                    document.querySelector('a-scene').appendChild(flowerModel);
-                }, 300);
-        */
-
-        // Replace ./data.json with your JSON feed
         fetch('assets/floorPic.json').then(response => {
             return response.json();
         }).then(floorPic => {
@@ -59,7 +10,7 @@ AFRAME.registerComponent('spawn-flowers', {
             var floorGap = -50;
             const flowerGap = 3;
             var index = 0;
-            self.interval2 = setInterval(() => {
+            self.interval = setInterval(() => {
                 if (index >= floorPic.length) {
                     return;
                 }
@@ -73,13 +24,13 @@ AFRAME.registerComponent('spawn-flowers', {
                 flowerModel.setAttribute('id', id);
                 var x = flowPosition.x * flowerGap;
                 var y = -floorGap;
-                var z = flowPosition.z * flowerGap*3 - gap;
+                var z = flowPosition.z * flowerGap * flowerGap - gap;
                 var temp = setInterval(() => {
                     if (y <= floorGap) {
                         clearInterval(temp);
                         return;
                     }
-                    y = y - 0.5;
+                    y = y - 0.25;
                     flowerModel.setAttribute('position', x + ' ' + y + ' ' + z);
                 }, 50);
                 flowerModel.setAttribute('position', x + ' ' + y + ' ' + z);
@@ -97,7 +48,6 @@ AFRAME.registerComponent('spawn-flowers', {
     },
     remove: function () {
         clearInterval(this.interval);
-        clearInterval(this.interval2);
     }
 });
 
