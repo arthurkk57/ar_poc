a3e152ecc882037dddce7168e2bdaedea63ac3a6
Arthur <xhk18@163.com>
2023-03-04 23:20:13 +0800

    test

--- poc/component.js
+++ poc/component.js
@@ -34,8 +34,8 @@ AFRAME.registerComponent('spawn-flowers', {
                     flowerModel.setAttribute('position', x + ' ' + y + ' ' + z);
                 }, 50);
                 flowerModel.setAttribute('position', x + ' ' + y + ' ' + z);
-                flowerModel.setAttribute('scale', '0.8 0.8 0.8');
-                flowerModel.setAttribute('material', 'opacity:1');
+                flowerModel.setAttribute('scale', '1 1 1');
+                // flowerModel.setAttribute('material', 'opacity:1');
                 flowerModel.setAttribute('rotation', '0 0 0');
                 flowerModel.setAttribute('animation__rotate', "property: rotation; to: 0 360 0; loop: true; dur: 5000");
                 document.querySelector('a-scene').appendChild(flowerModel);
@@ -44,6 +44,36 @@ AFRAME.registerComponent('spawn-flowers', {
         }).catch(err => {
             // Do something for an error here
         });
+        setTimeout(() => {
+            clearInterval(this.interval2);
+            fetch('assets/position/backgroundPic.json').then(response => {
+                return response.json();
+            }).then(bgPic => {
+                console.log(floorPic);
+                const frontGap = 50;
+                const upGap = 30;
+                const budGap = 3;
+                var index = 0;
+                for (var index = 0; index < bgPic.length; index++) {
+                    var pos = floorPic[index];
+                    var x = pos.x * budGap;
+                    var y = pos.y * budGap + upGap;
+                    var z = -frontGap;
+
+                    var budModel = document.createElement('a-entity');
+                    const modelId = Math.floor(Math.random() * 4) + 1;
+                    budModel.setAttribute('gltf-model', '#bud-' + modelId);
+                    budModel.setAttribute('position', x + ' ' + y + ' ' + z);
+                    budModel.setAttribute('scale', '1 1 1');
+                    // budModel.setAttribute('material', 'opacity:1');
+                    budModel.setAttribute('rotation', '0 0 0');
+                    budModel.setAttribute('animation__rotate', "property: rotation; to: 0 360 0; loop: true; dur: 5000");
+                    document.querySelector('a-scene').appendChild(budModel);
+                }
+            }).catch(err => {
+                // Do something for an error here
+            });
+        }, 5000);
     },
     remove: function () {
         clearInterval(this.interval);
